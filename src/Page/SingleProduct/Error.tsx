ab527e722004a07bfbd50e543fb677562ebe1625
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-08 23:33:50 +0545

    Product Not Found Page Down

--- src/Page/SingleProduct/Error.tsx
+++ src/Page/SingleProduct/Error.tsx
@@ -1,9 +1,10 @@
-
+import NoItem from '../../assets/No Item.svg'
 
 function Error() {
   return (
-    <div>
-      
+    <div className="flex flex-col w-[100%] justify-center items-center md:gap-5  xs:gap-4 gap-3 md:px-[5%] px-[3%] bg-background py-5 overflow-y-hidden">
+        <img src={NoItem} alt='No Item' className='sm:w-[350px] w-[300px]'/>
+        <h4 className='translate-y-[-100px] text-lg font-mb text-text-500'>No Product Found</h4>
     </div>
   )
 }
