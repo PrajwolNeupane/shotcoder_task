250058d9951330e55901c056efe8728e9353f7f3
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-09 13:28:33 +0545

    Working on Delete From Cart

--- src/Page/Cart/Page.tsx
+++ src/Page/Cart/Page.tsx
@@ -1,9 +1,75 @@
+import { CartItemInteface } from "../../Interface";
+import { useAppSelector } from "../../app/store"
+import { GoTrash } from "react-icons/go";
 
 
 function Page() {
+
+  const { cart } = useAppSelector((state) => state.Cart);
+
   return (
-    <div>
-      
+    <div className="flex flex-col w-[100%] justify-center md:gap-5  xs:gap-4 gap-3 md:px-[5%] px-[3%] sm:py-[30px] py-[15px] bg-background">
+      <h2 className='text-md font-sb text-text-500 mt-3'>Shopping Cart</h2>
+      <div className="flex flex-row w-[100%] gap-5 min-h-[465px] items-start">
+        <table className="bg-white w-[70%] border-spacing-[20px] border-separate rounded-[5px] shadow-md">
+          <thead>
+            <tr>
+              <th className="w-[5%] text-2xs font-sb text-text-500 text-start">S.N</th>
+              <th className="w-[50%] text-2xs font-sb text-text-500 text-start">Product Detail</th>
+              <th className="w-[10%] text-2xs font-sb text-text-500 text-start">Price</th>
+              <th className="w-[10%] text-2xs font-sb text-text-500 text-start">Quantity</th>
+              <th className="w-[10%] text-2xs font-sb text-text-500 text-start"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              cart?.map((curr: CartItemInteface | null, indx: number) => (
+                <tr>
+                  <td>
+                    {indx + 1}
+                  </td>
+                  <td className="flex flex-row gap-3">
+                    <img src={curr?.image} className="w-[15%] aspect-[1/1.2] object-contain" />
+                    <h3 className="text-3xs font-mb leading-[120%] text-text-500">{curr?.title}
+                      <br />
+                      <span className="text-3xs font-r leading-[150%] text-text-300">{curr?.description}</span>
+                    </h3>
+
+                  </td>
+                  <td className="text-3xs font-mb leading-[120%] text-text-500">$ {Number(curr?.price) * curr?.quantity!}</td>
+                  <td className="text-3xs font-mb leading-[120%] text-text-500">{curr?.quantity}</td>
+                  <td><button><GoTrash className="text-sm font-mb leading-[120%] text-text-500" /></button></td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+        <div className="flex flex-col bg-white w-[30%] h-[300px] border-spacing-[20px] border-separate rounded-[5px] shadow-md px-4 py-0">
+          <h2 className='text-md font-sb text-text-500 mt-3 leading-[120%]'>Cart Total</h2>
+          <table className="w-[90%]">
+            <tbody>
+              <tr>
+                <td>Total</td>
+                <td>{cart.length}</td>
+              </tr>
+              <tr >
+                <td className="w-[100%] h-[1px] bg-text-200 opacity-1"></td>
+              </tr>
+              <tr>
+                <td>Sub Total</td>
+                <td>{cart.length}</td>
+              </tr>
+              <tr>
+                <td>Delivery Charge</td>
+                <td>$50</td>
+              </tr>
+              <tr >
+                <td className="w-[100%] h-[1px] bg-text-200 opacity-1"></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   )
 }
